0c16343d715017d9ca0427b2693842c5e33462f1
Drew Rawitz <rawitz@sanbornagency.com>
2018-11-21 21:28:32 -0700

    scoreboard styling and functionality

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,119 @@
 <template>
   <div id="app">
-    <Scoreboard />
+    <Scoreboard
+      v-if="team1 && team2"
+      :team1="team1"
+      :team2="team2"
+      :team1Score="team1Score"
+      :team2Score="team2Score"
+      :period="period"
+      :timeRemaining="timeRemaining"
+      :powerplay="powerplay"
+      :powerplayRemaining="powerplayRemaining"
+    />
+
+    <div>
+      <button @click="resetGame">Reset Game</button>
+      <button @click="startTime">Start Time</button>
+      <button @click="stopTime">Stop Time</button>
+    </div>
+    <div>
+      <h1>Team 1</h1>
+      <select v-model="team1">
+        <option v-for="team in teams" :key="team.teamID" :value="team">{{ team.name }}</option>
+      </select>
+      <button @click="addGoal(1)">Add Goal</button>
+      <button @click="startPowerplay(1)">Start Powerplay</button>
+      <button @click="stopPowerplay()">Stop Powerplay</button>
+    </div>
+    <div>
+      <h1>Team 2</h1>
+      <select v-model="team2">
+        <option v-for="team in teams" :key="team.teamID" :value="team">{{ team.name }}</option>
+      </select>
+      <button @click="addGoal(2)">Add Goal</button>
+      <button @click="startPowerplay(2)">Start Powerplay</button>
+      <button @click="stopPowerplay()">Stop Powerplay</button>
+    </div>
   </div>
 </template>
 
 <script>
+import teams from '@/data/teams.json';
 import Scoreboard from '@/components/Scoreboard';
 
 export default {
   name: 'app',
   components: {
     Scoreboard,
+  },
+  data() {
+    return {
+      teams,
+      team1: teams[Math.floor(Math.random() * teams.length)],
+      team2: teams[Math.floor(Math.random() * teams.length)],
+      team1Score: 0,
+      team2Score: 0,
+      period: 1,
+      timeRemaining: 900,
+      timeInterval: null,
+      powerplay: 0,
+      powerplayRemaining: 0,
+    }
+  },
+  methods: {
+    startPowerplay(team) {
+      if(team !== 1 && team !== 2) return;
+
+      this.powerplay = team;
+      this.powerplayRemaining = 120;
+      this.stopTime();
+    },
+    stopPowerplay() {
+      this.powerplay = 0;
+    },
+    addGoal(team) {
+      if(team !== 1 && team !== 2) return;
+
+      if(team === 1) {
+        this.team1Score = this.team1Score + 1;
+      }
+
+      if(team === 2) {
+        this.team2Score = this.team2Score + 1;
+      }
+
+      this.stopTime();
+    },
+    startTime() {
+      if (this.timeInterval) return;
+
+      this.timeInterval = setInterval(() => {
+        this.timeRemaining = this.timeRemaining - 1;
+
+        if(this.powerplay) {
+          if(this.powerplayRemaining > 0) {
+            this.powerplayRemaining = this.powerplayRemaining - 1;
+          } else {
+            this.powerplay = 0;
+          }
+        }
+      }, 1000);
+    },
+    stopTime() {
+      this.timeInterval = clearInterval(this.timeInterval);
+    },
+    resetGame() {
+      this.period = 1;
+      this.team1Score = 0;
+      this.team2Score = 0;
+      this.timeRemaining = 900;
+      this.powerplay = 0;
+      this.powerplayRemaining = 120;
+
+      // stop time interval
+      this.stopTime();
+    }
   }
 }
 </script>
--- src/components/Scoreboard.vue
+++ src/components/Scoreboard.vue
@@ -1,99 +1,156 @@
 <template>
-  <section class="c-scoreboard">
-    <div class="c-scoreboard__group team1">
-      <div class="c-scoreboard__team">SJ</div>
-      <div class="c-scoreboard__score">2</div>
-    </div>
-    <div class="c-scoreboard__group team2">
-      <div class="c-scoreboard__team">EDM</div>
-      <div class="c-scoreboard__score">1</div>
-      <div class="c-scoreboard__powerplay">
+  <section class="scoreboard">
+    <div class="group group--team1" :style="`background-color: ${team1.color}; background-image: url(${team1.logo})`">
+      <div class="team">{{ team1.teamID }}</div>
+      <div class="score">{{ team1Score }}</div>
+      <div v-if="powerplay === 1" class="powerplay">
         <span>Power Play</span>
-        <span>1:37</span>
+        <span>{{ getTime(this.powerplayRemaining) }}</span>
       </div>
     </div>
-    <div class="c-scoreboard__group time">
-      <div class="c-scoreboard__period">1st</div>
-      <div class="c-scoreboard__time">8:15</div>
+    <div class="group group--team2" :style="`background-color: ${team2.color}; background-image: url(${team2.logo})`">
+      <div class="team">{{ team2.teamID }}</div>
+      <div class="score">{{ team2Score }}</div>
+      <div v-if="powerplay === 2" class="powerplay">
+        <span>Power Play</span>
+        <span>{{ getTime(this.powerplayRemaining) }}</span>
+      </div>
+    </div>
+    <div class="group group--time">
+      <div class="period">1st</div>
+      <div class="time">{{ getTime(this.timeRemaining) }}</div>
     </div>
   </section>
 </template>
 
+<script>
+  export default {
+    props: {
+      team1: Object,
+      team2: Object,
+      team1Score: {
+        type: Number,
+        default: 0,
+      },
+      team2Score: {
+        type: Number,
+        default: 0,
+      },
+      period: {
+        type: Number,
+        default: 1,
+      },
+      timeRemaining: {
+        type: Number,
+        default: 900
+      },
+      powerplay: {
+        type: Number,
+        default: 0,
+        validator: function(value) {
+          return [0, 1, 2].indexOf(value) !== -1;
+        }
+      },
+      powerplayRemaining: {
+        type: Number,
+        default: 120,
+      },
+    },
+    methods: {
+      getTime(time) {
+        const minutes = Math.floor(time / 60);
+        let seconds = time - minutes * 60;
+
+        if(seconds < 10) {
+          seconds = `0${seconds}`;
+        }
+
+        return `${minutes}:${seconds}`;
+      }
+    }
+  }
+</script>
+
 <style scoped>
-.c-scoreboard {
+.scoreboard {
   color: rgba(255, 255, 255, 0.9);
-  height: 60px;
+  /* height: 60px; */
   font-size: 30px;
   display: inline-flex;
   align-items: center;
-  outline: 2px solid #4a4a4a;
-
-  &__group {
-    position: relative;
-    display: flex;
-    align-items: center;
-    align-self: stretch;
-    box-shadow: inset 0 18px 0 rgba(255, 255, 255, 0.2);
-    background-position: left center;
-    background-size: 60px;
-    background-repeat: no-repeat;
-    padding-left: 50px;
-  }
-
-  &__team {
-    padding: 0 20px;
-    text-transform: uppercase;
-    font-size: 35px;
-  }
-
-  &__score {
-    background: rgba(0, 0, 0, 0.25);
-    align-self: stretch;
-    display: flex;
-    align-items: center;
-    padding: 0 20px;
-    font-weight: 700;
-    font-size: 50px;
-  }
-
-  &__time {
-    padding-left: 20px;
-  }
-
-  &__powerplay {
-    position: absolute;
-    top: 100%;
-    left: -2px;
-    width: calc(100% + 2px);
-    background-color: inherit;
-    z-index: 100;
-    border-top: 0;
-    display: flex;
-    justify-content: space-between;
-    font-size: 18px;
-    font-weight: 700;
-    text-transform: uppercase;
-    padding: 10px;
-    letter-spacing: -0.3px;
-    border: 2px solid rgba(0, 0, 0, 0.25);
-    border-top: 0;
-  }
+  outline: 2px solid var(--scoreboard-border);
+  margin-bottom: 100px;
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.2);
 }
 
-.team1 {
-  background-color: #1c4048;
-  background-image: url(../assets/img/tl_sharks.png);
+.group {
+  position: relative;
+  display: flex;
+  align-items: center;
+  align-self: stretch;
+  box-shadow: inset 0 18px 0 rgba(255, 255, 255, 0.2);
+  background-position: left 10px center;
+  background-size: 55px auto;
+  background-repeat: no-repeat;
+  padding-left: 60px;
 }
 
-.team2 {
-  background-color: #da6d17;
-  background-image: url(../assets/img/tl_oilers.png);
+.team {
+  padding: 0 20px;
+  padding-right: 10px;
+  text-transform: uppercase;
+  font-size: 35px;
+}
+
+.score {
+  align-self: stretch;
+  display: flex;
+  align-items: center;
+  padding: 0 20px;
+  font-weight: 700;
+  font-size: 50px;
 }
 
 .time {
+  padding-left: 20px;
+}
+
+.powerplay {
+  position: absolute;
+  top: 100%;
+  left: -2px;
+  width: calc(100% + 4px);
+  background-color: inherit;
+  z-index: 100;
+  border-top: 0;
+  display: flex;
+  justify-content: space-between;
+  font-size: 18px;
+  font-weight: 700;
+  text-transform: uppercase;
+  padding: 10px;
+  letter-spacing: -0.3px;
+  /* border: 2px solid rgba(0, 0, 0, 0.25); */
+  border: 2px solid var(--scoreboard-border);
+  border-top: 0;
+}
+
+.group--team1 {
+  background-color: #1c4048;
+  background-image: url(../assets/img/tl_sharks.png);
+  border-right: 2px solid var(--scoreboard-border);
+}
+
+.group--team2 {
+  background-color: #da6d17;
+  background-image: url(../assets/img/tl_oilers.png);
+  border-right: 2px solid var(--scoreboard-border);
+}
+
+.group--time {
   padding: 0 20px;
   background: #ececec;
-  color: #4a4a4a;
+  color: var(--scoreboard-border);
   font-weight: 700;
   box-shadow: inset 0 18px 0 rgba(255, 255, 255, 0.9);
 }
